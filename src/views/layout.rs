c89d47226f6d395d8543fba5efda33582e1ba0dc
Rafael Epplée <hello@rafa.ee>
2024-09-25 09:34:08 +0200

    Add htmf
    Remove htmf lifetimes

--- src/views/layout.rs
+++ src/views/layout.rs
@@ -1,8 +1,12 @@
+use htmf::prelude::*;
+
 use crate::{
     authentication::AuthUser,
-    db::{self, layout::AuthedInfo, AppTx},
+    db::{self, layout::AuthedInfo, AppTx, List},
     response_error::ResponseResult,
 };
+
+use super::base_document::base_document;
 
 pub struct Template {
     pub authed_info: Option<AuthedInfo>,
@@ -19,4 +23,97 @@ impl Template {
             authed_info: auth_info,
         })
     }
+}
+
+pub fn layout(children: Element, layout: &Template) -> Element {
+    base_document(div(class("flex-row-reverse h-full sm:flex")).with([
+        main_(class("sm:overflow-y-auto sm:grow")).with(children),
+        match &layout.authed_info {
+            Some(info) => sidebar(info),
+            None => fragment(),
+        },
+    ]))
+}
+
+fn sidebar(authed_info: &AuthedInfo) -> Element {
+    aside([
+        id("nav"),
+        class(
+            "bg-neutral-900 sm:max-w-[18rem] sm:w-1/3 sm:max-h-full flex flex-col \
+             sm:flex-col-reverse sm:border-r border-neutral-700 border-t sm:border-t-0",
+        ),
+    ])
+    .with([
+        div(class("sm:overflow-y-auto sm:flex-1")).with([lists_header(), lists(authed_info)]),
+        header(class(
+            "sticky bottom-0 flex justify-between p-2 leading-8 bg-neutral-900",
+        ))
+        .with([
+            a([
+                href("/"),
+                class("px-2 font-bold rounded  hover:bg-neutral-800"),
+            ])
+            .with(&authed_info.user_description),
+            form([action("/logout"), method("post")]).with(
+                button(class("rounded px-3  text-neutral-400 hover:bg-neutral-800")).with("Logout"),
+            ),
+        ]),
+    ])
+}
+
+fn lists_header() -> Element {
+    div(class(
+        "sticky top-0 flex items-center justify-between px-2 pt-2 sm:top-0 bg-neutral-900",
+    ))
+    .with([
+        h3(class(
+            "px-2 py-1 text-sm font-bold tracking-tight text-neutral-400",
+        ))
+        .with("Lists"),
+        a([
+            href("/lists/create"),
+            class("block px-3 text-xl rounded hover:bg-neutral-800 text-neutral-400"),
+        ])
+        .with("+"),
+    ])
+}
+
+fn lists(authed_info: &AuthedInfo) -> Element {
+    let lists = authed_info.lists.iter();
+    ul(class("pb-2")).with([
+        li([]).with(
+            a([
+                class(
+                    "block px-4 py-1 overflow-hidden text-ellipsis whitespace-nowrap \
+                     hover:bg-neutral-800",
+                ),
+                href("/bookmarks/unsorted"),
+            ])
+            .with("Unsorted bookmarks"),
+        ),
+        fragment().with(lists.map(list_item).collect::<Vec<_>>()),
+        li([]).with(
+            a([
+                class(
+                    "block px-4 py-1 overflow-hidden text-ellipsis whitespace-nowrap \
+                     hover:bg-neutral-800 text-neutral-400",
+                ),
+                href("/lists/unpinned"),
+            ])
+            .with("Unpinned lists"),
+        ),
+    ])
+}
+
+fn list_item(list: &List) -> Element {
+    li([]).with(
+        a([
+            class(
+                "block px-4 py-1 overflow-hidden text-ellipsis whitespace-nowrap \
+                 hover:bg-neutral-800",
+            ),
+            href(format!("/lists/{}", list.id)),
+        ])
+        .with(&list.title),
+    )
 }
