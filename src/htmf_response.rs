fbeb22a0bbf58f78da108173554f0a0cbe55c6be
Rafael Epplée <hello@rafa.ee>
2025-03-10 15:21:35 +0100

    Refactor HtmfResponse implementation

--- src/htmf_response.rs
+++ src/htmf_response.rs
@@ -6,18 +6,15 @@ use crate::response_error::ResponseError;
 pub struct HtmfResponse(pub htmf::element::Element);
 
 impl IntoResponse for HtmfResponse {
-  #[cfg(debug_assertions)]
   fn into_response(self) -> axum::response::Response {
-    let maybe_html = self.0.to_html_pretty();
-    if let Ok(html) = maybe_html {
-      Html(html).into_response()
-    } else {
-      ResponseError::Anyhow(anyhow!("Failed to serialize htmf element")).into_response()
+    if cfg!(not(debug_assertions)) {
+      return Html(self.0.to_html()).into_response();
     }
-  }
 
-  #[cfg(not(debug_assertions))]
-  fn into_response(self) -> axum::response::Response {
-    Html(self.0.to_html()).into_response()
+    let Ok(html) = self.0.to_html_pretty() else {
+      return ResponseError::Anyhow(anyhow!("Failed to serialize htmf element")).into_response();
+    };
+
+    Html(html).into_response()
   }
 }
