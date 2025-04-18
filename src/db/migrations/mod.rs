5d2e8ec3231e2bbc69bafcc2dd2eb9e341c5d12f
Rafael Epplée <hello@rafa.ee>
2025-04-18 17:31:25 +0200

    Make user -> AP user relation mandatory

--- src/db/migrations/mod.rs
+++ src/db/migrations/mod.rs
@@ -6,11 +6,16 @@ use anyhow::Result;
 use sqlx::PgTransaction;
 use url::Url;
 
+mod generate_missing_ap_users;
+
 #[allow(clippy::inconsistent_digit_grouping)]
 pub async fn run_before(
     previous_migration: &sqlx::migrate::Migration,
     tx: &mut PgTransaction<'_>,
     base_url: &Url,
 ) -> Result<()> {
+    if previous_migration.version == 2025_10_14_160454 {
+        generate_missing_ap_users::migrate(tx, base_url).await?;
+    }
     Ok(())
 }
